cea2e5d0df609b3121fd1971a1d11a0cd86ab24f
nickjo24 <njohnso2@chicagobooth.edu>
2023-01-21 18:36:26 +0000

    Finished dynamic RPS game

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,43 @@ class ApplicationController < ActionController::Base
   # Add your actions below this line
   # ================================
 
+  def homepage
+    render({ :template => "game_templates/rules.html.erb",
+             :layout => "wrapper.html.erb" })
+  end
+
+  def play_rock
+    render({ :template => "game_templates/user_rock.html.erb",
+             :layout => "wrapper.html.erb" })
+  end
+
+  def play_paper
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move == "scissors"
+      @outcome = "lost"
+    end
+
+    render({ :template => "game_templates/user_paper.html.erb",
+             :layout => "wrapper.html.erb" })
+  end
+
+  def play_scissors
+    @comp_move = ["rock", "paper", "scissors"].sample
+
+    if @comp_move == "rock"
+      @outcome = "lost"
+    elsif @comp_move == "paper"
+      @outcome = "won"
+    elsif @comp_move == "scissors"
+      @outcome = "tied"
+    end
+
+    render({ :template => "game_templates/user_scissors.html.erb",
+             :layout => "wrapper.html.erb" })
+  end
 end
